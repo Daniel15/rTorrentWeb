487df1db5bc77d8691b89093794702daf20312a2
Joseph Stubberfield <stubbers@stubbers101.net>
2010-01-16 06:01:53 +0000

    Close #85 - Added ability to change upload and download rate limits for admin users - Added basic validation to the General Settings tab

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -55,24 +55,62 @@ defined('SYSPATH') OR die('No direct access allowed.');
 	 */
 	public function index()
 	{
+		$rtorrent = new Rtorrent;
+		
+		$this->template->title = 'General Settings';
+		$page = View::factory('admin/index');	
+				
+		
 		// Did they submit the page?
 		if ($this->input->post('submit'))
 		{
-			// Let's go through all the possible settings
-			foreach (array('rpcurl', 'metadata_dir', 'torrent_dir') as $key)
+			// Validate the form
+			$post = Validation::factory($_POST)
+				->pre_filter('trim')
+				->add_rules('rpcurl', 'required', 'standard_text')
+				->add_rules('metadata_dir', 'required', 'standard_text')
+				->add_rules('torrent_dir', 'required', 'standard_text')
+				->add_rules('down_rate', 'required', 'digit')
+				->add_rules('up_rate', 'required', 'digit');
+			
+			// Is it valid
+			if ($post->validate())
 			{
-				// If it's not set, skip it
-				if (null === ($value = $this->input->post($key)))
-					continue;
-				// Save it
-				// TODO: Validation
-				$this->config->set($key, $value);
+				// Let's go through all the possible settings
+				foreach (array('rpcurl', 'metadata_dir', 'torrent_dir') as $key)
+				{
+					// If it's not set, skip it
+					if (null === ($value = $this->input->post($key)))
+						continue;
+					// Save it
+					// TODO: Better Validation
+					$this->config->set($key, $value);
+				}
+				
+				$rtorrent->do_call('set_upload_rate', $this->input->post('up_rate') * 1024);
+				$rtorrent->do_call('set_download_rate', $this->input->post('down_rate') * 1024);
+				
+				$this->template->top_message = 'Your changes were saved';
 			}
-			$this->template->top_message = 'Your changes were saved';
+			else
+			{
+				$page->errors = $post->errors();
+			}
+			
+			$page->settings = $post;
+		}
+		else
+		{
+			$page->settings = array(
+				'rpcurl' => $this->config->get('rpcurl'),
+				'metadata_dir' => $this->config->get('metadata_dir'),
+				'torrent_dir' => $this->config->get('torrent_dir'),
+				'up_rate' => $rtorrent->do_call('get_throttle_up_max', '') / 1024,
+				'down_rate' => $rtorrent->do_call('get_throttle_down_max', '') / 1024
+			);
 		}
 		
-		$this->template->title = 'General Settings';
-		$this->template->content = View::factory('admin/index');
+		$this->template->content = $page;
 	}
 	
 	/** 
--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -22,22 +22,51 @@
 defined('SYSPATH') OR die('No direct access allowed.'); ?>
 
 <p>This page will let you configure basic settings relating to your rTorrentWeb installation.</p>
+<?php
+// Any errors?
+if (!empty($errors))
+{
+	echo '
+	<div class="errors">
+		The following errors were encountered:
+		<ul>';
+	
+	foreach ($errors as $key => $error)
+		echo '
+			<li><strong>', $key, '</strong>: ', $error, '</li>';
+	echo '
+		</ul>
+	</div>';
+}
+?>
 
 <?php echo form::open('admin', array('id' => 'settings')) ?>
+<h2>rTorrentWeb Settings</h2>
 	<p>
 		<label for="rpcurl">rTorrent RPC URL:</label>
-		<input type="text" name="rpcurl" id="rpcurl" size="50" value="<?php echo $this->config->get('rpcurl'); ?>" /><br />
+		<input type="text" name="rpcurl" id="rpcurl" size="50" value="<?php echo $settings['rpcurl']; ?>" /><br />
 		<small>This is the URL to rTorrent's XMLRPC. If you followed the XMLRPC setup instructions, this will usually be at http://localhost/RPC2/.</small>
 	</p>
 	<p>
 		<label for="metadata_dir">Metadata Directory:</label>
-		<input type="text" name="metadata_dir" id="metadata_dir" size="80" value="<?php echo $this->config->get('metadata_dir'); ?>" /><br />
+		<input type="text" name="metadata_dir" id="metadata_dir" size="80" value="<?php echo $settings['metadata_dir']; ?>" /><br />
 		<small>This is the directory .torrent files are stored in.</small>
 	</p>
 	<p>
 		<label for="torrent_dir">Torrent Directory:</label>
-		<input type="text" name="torrent_dir" id="torrent_dir" size="80" value="<?php echo $this->config->get('torrent_dir'); ?>" /><br />
+		<input type="text" name="torrent_dir" id="torrent_dir" size="80" value="<?php echo $settings['torrent_dir']; ?>" /><br />
 		<small>This is the directory the torrent data (ie. the files actually inside the torrent) is stored in. Each user will get their own directory underneath here.</small>
+	</p>
+	
+<h2>rTorrent Settings</h2>
+	<p>
+		<label for="up_rate">Limit Upload Speed</label>
+		<input type="text" name="up_rate" id="up_rate" size="2" value="<?php echo $settings['up_rate']; ?>" />
+		<small>kb/s</small>
+		<label for="down_rate">Limit Download Speed</label>
+		<input type="text" name="down_rate" id="down_rate" size="2" value="<?php echo $settings['down_rate']; ?>" />
+		<small>kb/s</small><br />
+		<small>This sets the hard server wide download/upload speed limits on rTorrent. This will apply to all users in total, not individually.</small>
 	</p>
 	
 	<p><input type="submit" name="submit" value="Save Changes" /></p>
